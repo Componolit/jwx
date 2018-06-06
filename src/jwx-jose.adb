98687bad374d419b6ffd8fc0202f552abf9f4991
Alexander Senier <senier@componolit.com>
2018-06-06 21:27:24 +0200

    Add missing file headers

--- src/jwx-jose.adb
+++ src/jwx-jose.adb
@@ -1,3 +1,14 @@
+--
+-- \brief  JOSE header decoding (RFC 7515, section 4)
+-- \author Alexander Senier
+-- \date   2018-06-06
+--
+-- Copyright (C) 2018 Componolit GmbH
+--
+-- This file is part of JWX, which is distributed under the terms of the
+-- GNU Affero General Public License version 3.
+--
+
 with JWX.JSON;
 with JWX.Base64;
 with JWX.Util;
--- src/jwx-lsc.adb
+++ src/jwx-lsc.adb
@@ -1,3 +1,14 @@
+--
+-- \brief  JWX binding to libsparkcrypto
+-- \author Alexander Senier
+-- \date   2018-06-06
+--
+-- Copyright (C) 2018 Componolit GmbH
+--
+-- This file is part of JWX, which is distributed under the terms of the
+-- GNU Affero General Public License version 3.
+--
+
 with JWX;
 
 package body JWX.LSC
