33b156844bf128ba2298e9c6bb3c62854bddcbf3
Alexander Senier <senier@componolit.com>
2018-06-24 13:38:12 +0200

    Implement command line parsing and pass-through

--- examples/authproxy.adb
+++ examples/authproxy.adb
@@ -10,6 +10,7 @@
 --
 
 with GNAT.Sockets; use GNAT.Sockets;
+with GNAT.Command_Line; use GNAT.Command_Line;
 with Ada.Text_IO; use Ada.Text_IO;
 
 with JWX.Stream_Auth;
@@ -26,21 +27,11 @@ is
    Issuer   : constant String := "https://cmpnlt-demo.eu.auth0.com/";
    Key_Data : String := Read_File ("tests/data/HTTP_auth_key.json");
 
-   Server_IP   : String   := "127.0.3.1";
-   Server_Port : constant := 8080;
+   Server_Address   : Sock_Addr_Type;
+   Upstream_Address : Sock_Addr_Type;
 
-   Server_Address : constant Sock_Addr_Type :=
-      (Family => Family_Inet,
-       Addr   => Inet_Addr (Server_IP),
-       Port   => Server_Port);
-
-   Upstream_IP   : String   := "127.0.0.1";
-   Upstream_Port : constant := 80;
-
-   Upstream_Address : constant Sock_Addr_Type :=
-      (Family => Family_Inet,
-       Addr   => Inet_Addr (Upstream_IP),
-       Port   => Upstream_Port);
+   -- Do not perform validation
+   Passthrough : Boolean := False;
 
    Error_HTML : constant String :=
       "<HTML><BODY><H1>Unauthorized request. Please login.</H1></BODY></HTML>";
@@ -82,7 +73,6 @@ is
       Server_Socket   : Socket_Type;
       Upstream_Socket : Socket_Type;
 
-      C         : Character;
       Read_Set  : Socket_Set_Type;
       Write_Set : Socket_Set_Type;
       Selector  : Selector_Type;
@@ -133,14 +123,17 @@ is
                      Buffer : String (1 .. Request.Size);
                   begin
                      String'Read (Stream (Server_Socket), Buffer);
-                     Time (Now);
-                     Auth := Authenticated (Buffer, Long_Integer (Now));
-                     if Auth /= Auth_OK
+                     if not Passthrough
                      then
-                        String'Write (Stream (Server_Socket), Error_Message (Error_HTML));
-                        Close_Socket (Upstream_Socket);
-                        Close_Socket (Server_Socket);
-                        exit;
+                        Time (Now);
+                        Auth := Authenticated (Buffer, Long_Integer (Now));
+                        if Auth /= Auth_OK
+                        then
+                           String'Write (Stream (Server_Socket), Error_Message (Error_HTML));
+                           Close_Socket (Upstream_Socket);
+                           Close_Socket (Server_Socket);
+                           exit;
+                        end if;
                      end if;
                      String'Write (Stream (Upstream_Socket), Buffer);
                   end;
@@ -158,7 +151,7 @@ is
                      Buffer : String (1 .. Request.Size);
                   begin
                      String'Read (Stream (Upstream_Socket), Buffer);
-                     if Auth = Auth_OK
+                     if Passthrough or Auth = Auth_OK
                      then
                         String'Write (Stream (Server_Socket), Buffer);
                      end if;
@@ -175,9 +168,81 @@ is
 
    end Proxy;
 
+   procedure Print_Usage
+   is
+   begin
+      Put_Line ("authproxy [--local LOCAL_ADDRESS] [--lport LOCAL_PORT] [--upstream UPSTREAM_ADDRESS] [--uport UPSTREAM_PORT] [--passthrough]");
+   end Print_Usage;
+
+   function Handle_Command_Line return Boolean
+   is
+      Server_IP     : access String := new String'("127.0.3.1");
+      Server_Port   : Port_Type     := 8080;
+      Upstream_IP   : access String := new String'("127.0.0.1");
+      Upstream_Port : Port_Type     := 80;
+      S : Character;
+   begin
+      loop
+         begin
+            S := Getopt ("-local= -lport= -upstream= -uport= -passthrough -help");
+         exception
+            when others =>
+               Print_Usage;
+               return False;
+         end;
+
+         case S
+         is
+            when '-' =>
+               if Full_Switch = "-local"
+               then
+                  Server_IP := new String'(Parameter);
+               elsif Full_Switch = "-lport"
+               then
+                  Server_Port := Port_Type'Value (Parameter);
+               elsif Full_Switch = "-upstream"
+               then
+                  Upstream_IP := new String'(Parameter);
+               elsif Full_Switch = "-uport"
+               then
+                  Upstream_Port := Port_Type'Value (Parameter);
+               elsif Full_Switch = "-passthrough"
+               then
+                  Put_Line ("WARNING: RUNNING IN *PASSTHROUGH* MODE!");
+                  Passthrough := True;
+               elsif Full_Switch = "-help"
+               then
+                  Print_Usage;
+                  return False;
+               end if;
+            when others =>
+               exit;
+         end case;
+      end loop;
+
+      Server_Address :=
+         (Family => Family_Inet,
+          Addr   => Inet_Addr (Server_IP.all),
+          Port   => Server_Port);
+
+      Upstream_Address :=
+         (Family => Family_Inet,
+          Addr   => Inet_Addr (Upstream_IP.all),
+          Port   => Upstream_Port);
+
+      return True;
+
+   end Handle_Command_Line;
+
    P : access Proxy;
 
 begin
+
+   if not Handle_Command_Line
+   then
+      return;
+   end if;
+
    Put_Line ("Forwarding " & Image (Server_Address) & " <=> " & Image (Upstream_Address));
    Initialize;
    Create_Socket (Socket => Server_Socket);
