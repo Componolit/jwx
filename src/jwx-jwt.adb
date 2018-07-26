e4f22333a13d125ab3057f02e374fc14b99a13e7
Alexander Senier <senier@componolit.com>
2018-07-26 20:06:38 +0200

    Proof JWT handling

--- src/jwx-jwt.adb
+++ src/jwx-jwt.adb
@@ -25,6 +25,12 @@ is
    begin
       Result := Result_Invalid;
 
+      if Data'Length >= Integer'Last - 2 or
+        Data'Length <= 0
+      then
+         return;
+      end if;
+
       J.Validate_Compact (Validate_Result);
       if Validate_Result /= J.Result_OK
       then
@@ -37,82 +43,99 @@ is
          return;
       end if;
 
+      pragma Assert (Data'Length < Integer'Last);
       declare
-         Data : constant String := J.Payload;
-         use Base64;
-         Len   : Natural;
-         Decoded    : Byte_Array (1 .. 3 * ((Data'Length + 3) / 4));
-         JSON_Input : String (1 .. Data'Length);
+         Len : Natural;
+         Payload : constant String := J.Payload;
       begin
-         Decode_Url (Data, Len, Decoded);
-         if Len = 0
+
+         if Payload'Length <= 0 or
+           Payload'Length >= Natural'Last / 9 or
+           Payload'Last >= Natural'Last - 4
          then
-            Result := Result_Invalid_Base64;
             return;
          end if;
 
-         Util.To_String (Decoded, JSON_Input);
-
          declare
-            Input : constant String := JSON_Input;
-            package Token is new JWX.JSON (Input);
-            use Token;
-            Match : Match_Type;
-            Value : Index_Type;
+            JSON_Input : String (1 .. Payload'Length + 2);
+            Decoded    : Byte_Array (1 .. 3 * ((Payload'Length + 3) / 4));
+            use Base64;
          begin
-            Parse (Match);
-            if Match /= Match_OK
+
+            Decode_Url (Payload, Len, Decoded);
+            if Len = 0
             then
-               return;
-            end if;
-            if Get_Kind /= Kind_Object
-            then
-               Result := Result_Invalid_Object;
+               Result := Result_Invalid_Base64;
                return;
             end if;
 
-            -- Check issuer
-            Result := Result_Invalid_Issuer;
-            Value := Query_Object ("iss");
-            if Value = End_Index
-            then
-               return;
-            end if;
+            pragma Assert (JSON_Input'Length >= 3 * ((Payload'Length + 3) / 4));
+            Util.To_String (Decoded, JSON_Input);
 
-            if Get_String (Value) /= Issuer
-            then
-               return;
-            end if;
+            declare
+               Input : constant String := JSON_Input;
+               package Token is new JWX.JSON (Input);
+               use Token;
+               Match : Match_Type;
+               Value : Index_Type;
+            begin
+               Parse (Match);
+               if Match /= Match_OK
+               then
+                  return;
+               end if;
+               if Get_Kind /= Kind_Object
+               then
+                  Result := Result_Invalid_Object;
+                  return;
+               end if;
 
-            -- Check audience
-            Result := Result_Invalid_Audience;
-            Value := Query_Object ("aud");
-            if Value = End_Index
-            then
-               return;
-            end if;
+               -- Check issuer
+               Result := Result_Invalid_Issuer;
+               Value := Query_Object ("iss");
+               if Value = End_Index
+               then
+                  return;
+               end if;
 
-            if Get_String (Value) /= Audience
-            then
-               return;
-            end if;
+               if Get_Kind (Value) /= Kind_String or else
+                 Get_String (Value) /= Issuer
+               then
+                  return;
+               end if;
 
-            -- Check expiration
-            Result := Result_Expired;
-            Value := Query_Object ("exp");
-            if Value = End_Index
-            then
-               return;
-            end if;
+               -- Check audience
+               Result := Result_Invalid_Audience;
+               Value := Query_Object ("aud");
+               if Value = End_Index
+               then
+                  return;
+               end if;
 
-            if Long_Integer (Get_Integer (Value)) < Now
-            then
-               return;
-            end if;
+               if Get_Kind (Value) /= Kind_String or else
+                 Get_String (Value) /= Audience
+               then
+                  return;
+               end if;
+
+               -- Check expiration
+               Result := Result_Expired;
+               Value := Query_Object ("exp");
+               if Value = End_Index
+               then
+                  return;
+               end if;
+
+               if Get_Kind (Value) /= Kind_Integer or else
+                 Long_Integer (Get_Integer (Value)) < Now
+               then
+                  return;
+               end if;
+             end;
          end;
 
          Result := Result_OK;
-         return ;
+         return;
       end;
 
    end Validate_Compact;
